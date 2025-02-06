8634a375c35b3b0d280fcdc44cd1f950b8e701c6
Andrei Shpileuski <shpilevskiy.aa@gmail.com>
2025-02-06 21:08:57 +0500

    init

--- src/app/features/solution/solution-page/solution-page.component.ts
+++ src/app/features/solution/solution-page/solution-page.component.ts
@@ -4,14 +4,18 @@ import {
   inject,
   Signal,
 } from '@angular/core';
-import { BackButtonComponent } from '@app/ui-kit/back-button/back-button.component';
 import { TranslatePipe } from '@ngx-translate/core';
 import { ITestTaskDescription } from '@core/entities/interfaces/test-task-description.interface';
 import { projectInfoStore } from '@core/data-access/state/project-info.store';
+import { MatIcon } from '@angular/material/icon';
+import { MatIconButton } from '@angular/material/button';
+import { Location } from '@angular/common';
+import { Router } from '@angular/router';
+import { from, take } from 'rxjs';
 
 @Component({
   selector: 'app-solution-page',
-  imports: [BackButtonComponent, TranslatePipe],
+  imports: [TranslatePipe, MatIcon, MatIconButton],
   templateUrl: './solution-page.component.html',
   styleUrl: './solution-page.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
@@ -19,4 +23,17 @@ import { projectInfoStore } from '@core/data-access/state/project-info.store';
 export class SolutionPageComponent {
   public testTaskDescription: Signal<ITestTaskDescription | null> =
     inject(projectInfoStore).testTask;
+
+  private readonly _location = inject(Location);
+  private readonly _router = inject(Router);
+
+  public back(): void {
+    if (window.history.length > 1) {
+      this._location.back();
+    } else {
+      from(this._router.navigate(['/']))
+        .pipe(take(1))
+        .subscribe();
+    }
+  }
 }
--- src/app/features/task/task-page/task-page.component.ts
+++ src/app/features/task/task-page/task-page.component.ts
@@ -4,16 +4,18 @@ import {
   inject,
   Signal,
 } from '@angular/core';
-import { BackButtonComponent } from '@app/ui-kit/back-button/back-button.component';
 import { TranslatePipe } from '@ngx-translate/core';
 import { ITestTaskDescription } from '@core/entities/interfaces/test-task-description.interface';
 import { projectInfoStore } from '@core/data-access/state/project-info.store';
-import { MatAnchor } from '@angular/material/button';
-import { RouterLink } from '@angular/router';
+import { MatAnchor, MatIconButton } from '@angular/material/button';
+import { Router, RouterLink } from '@angular/router';
+import { MatIcon } from '@angular/material/icon';
+import { from, take } from 'rxjs';
+import { Location } from '@angular/common';
 
 @Component({
   selector: 'app-task-page',
-  imports: [BackButtonComponent, TranslatePipe, MatAnchor, RouterLink],
+  imports: [TranslatePipe, MatAnchor, RouterLink, MatIconButton, MatIcon],
   templateUrl: './task-page.component.html',
   styleUrl: './task-page.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
@@ -21,4 +23,17 @@ import { RouterLink } from '@angular/router';
 export class TaskPageComponent {
   public testTaskDescription: Signal<ITestTaskDescription | null> =
     inject(projectInfoStore).testTask;
+
+  private readonly _location = inject(Location);
+  private readonly _router = inject(Router);
+
+  public back(): void {
+    if (window.history.length > 1) {
+      this._location.back();
+    } else {
+      from(this._router.navigate(['/']))
+        .pipe(take(1))
+        .subscribe();
+    }
+  }
 }
