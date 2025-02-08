3d1b34e3ba1a1662f8d3b1dcd5c201a1d586f60a
Andrei Shpileuski <shpilevskiy.aa@gmail.com>
2025-02-08 06:57:54 +0500

    refactoring

--- src/app/core/ui/components/header/header.component.ts
+++ src/app/core/ui/components/header/header.component.ts
@@ -1,13 +1,14 @@
 import { ChangeDetectionStrategy, Component, input } from '@angular/core';
 import { NgOptimizedImage } from '@angular/common';
 import { IAuthor } from '@app/domain/entities/interfaces/author.interface';
+import {MatTooltip} from '@angular/material/tooltip';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
-  imports: [NgOptimizedImage],
+  imports: [NgOptimizedImage, MatTooltip],
 })
 export class HeaderComponent {
   public author = input.required<IAuthor | null>();
