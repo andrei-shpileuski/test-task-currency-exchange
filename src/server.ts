adf54c744c447d6ca1c36d1d63765ee1ddc133ae
Andrei Shpileuski <shpilevskiy.aa@gmail.com>
2025-02-13 12:14:03 +0500

    refactoring

--- src/server.ts
+++ src/server.ts
@@ -2,11 +2,11 @@ import {
   AngularNodeAppEngine,
   createNodeRequestHandler,
   isMainModule,
-  writeResponseToNodeResponse,
 } from '@angular/ssr/node';
 import express from 'express';
 import { dirname, resolve } from 'node:path';
 import { fileURLToPath } from 'node:url';
+import { defaultLanguage } from '@core/entities/constants/default-language.const';
 
 const serverDistFolder = dirname(fileURLToPath(import.meta.url));
 const browserDistFolder = resolve(serverDistFolder, '../browser');
@@ -22,13 +22,23 @@ app.use(
   }),
 );
 
-app.use('/**', (req, res, next) => {
-  angularApp
-    .handle(req)
-    .then((response) =>
-      response ? writeResponseToNodeResponse(response, res) : next(),
-    )
-    .catch(next);
+app.use('/**', async (req, res, next) => {
+  try {
+    const response = await angularApp.handle(req);
+
+    if (!response) {
+      return next();
+    }
+
+    let html = await response.text();
+    html = html.replace('<html', `<html lang="${defaultLanguage}"`);
+
+    res.status(response.status);
+    response.headers.forEach((value, key) => res.setHeader(key, value));
+    res.send(html);
+  } catch (error) {
+    next(error);
+  }
 });
 
 if (isMainModule(import.meta.url)) {
